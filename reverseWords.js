c03bebf4440cec45b934969cd9c3ba81de532657
GreenRoot <1.ooo.ooo@bk.ru>
2021-09-01 23:40:15 +0300

    reverseWords

---
+++ reverseWords.js
@@ -0,0 +1,11 @@
+// Complete the solution so that it reverses all of the words within the string passed in.
+// Example:
+// "The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The"
+
+const reverseWords = str =>	str
+	.split(' ')
+	.reverse()
+	.join(' ');
+
+let str = "hello world!"
+console.log(reverseWords(str))
